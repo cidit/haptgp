9ae7d569c1e0a546b21c549ae3d8d56e6aa3fc4b
cidit <44065673+cidit@users.noreply.github.com>
2025-10-04 14:42:15 -0400

    added tap

--- src/main.py
+++ src/main.py
@@ -1,5 +1,32 @@
+from tap import Tap
+
+
+def get_pyproject():
+    import tomllib
+    with open('pyproject.toml', 'rb') as f:
+        pyproject = tomllib.load(f)
+        return pyproject
+
+
+class ArgumentParser(Tap):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        pyproject = get_pyproject()
+        self.description = f"""{pyproject["project"]["name"]} v{pyproject["project"]["version"]}
+                            {pyproject["project"]["description"]}
+                            """
+    hello: bool = False
+
+
 def main():
-    print("Hello from haptgp!")
+    
+    args = ArgumentParser().parse_args()
+    
+    if args.hello:
+        print("hello from haptgp!")
+        
+    while True:
+        pass
 
 
 if __name__ == "__main__":
